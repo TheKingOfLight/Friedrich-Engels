7548c9b7f165b3dae4dec9accb4b0fc5cde7dd17
TheKingOfLight <72839891+TheKingOfLight@users.noreply.github.com>
2020-10-21 20:57:40 +0200

    Add files via upload
    Update: Rollen everybody und rekrut richtig vergeben

---
+++ Friedrich Engels/core-01.02.02.py
@@ -0,0 +1,104 @@
+'''-----------------------------------------------------------------
+Python Bot "Friedrich Engels" Version 01.02.02
+von Mark Rygielski
+
+Aufgabe:
+Unangemessenes Verhalten im Chat erkennen und bestrafen
+Bedeutung der Aufgabe in dieser Version
+Unangemessenes Verhalten: ein geschriebenes Wort ist auf einer Liste
+Bestrafen: im Chat aufmerksam machen
+Aufgabe:
+Im Chat Reaction von Usern auf eine Nachricht erkennen und entsprechende Rolle
+vergeben
+
+Externe Datein (im selben Ordner):
+- .ent mit dem Token
+-----------------------------------------------------------------'''
+
+
+#Setup
+#------------------------------------------------------------------
+
+#importiere Bibliotheken:
+import discord, os, re
+from discord.utils import get
+from dotenv import load_dotenv
+
+#importiere Python skripte
+from moderation.testmessage import testmessage
+from role_management.on_reaction_add_role import on_reaction_add_role
+from setup_bot import welcome_client
+
+#importiere Benachrichtigungen
+from textoutput.user_messages import badmessage
+from textoutput.user_messages import bad_message_detected
+from textoutput.error_messages import error1
+
+#importiere Einstellungen
+from serverconfic.server import comandprefix
+from serverconfic.server import give_text_in_bot_channel
+comand_prefix = str(comandprefix)
+
+#importiere Token mithilfe von dotenv:
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+#------------------------------------------------------------------
+
+
+
+def main():
+    #client instanz erzeugen:
+    client = discord.Client()
+
+    #Stellt den Bot entsprechend ein, gibt Systemnachrichten aus
+    welcome_client(client)
+
+    
+    @client.event
+    async def on_disconnect():
+        #Läuft, die Verbindung zum Server abbricht
+        print("Bot has logged off")
+
+        
+    @client.event
+    async def on_message(message):
+        #läuft, wenn der Bot eine Nachricht erhält
+        if message.author == client.user:
+            #testet, ob der Bot die Nachricht selber gesendet hat
+            return
+        
+        if message.content.startswith(comand_prefix):
+            #prüft, ob ein Befehl gegeben wurde
+            print ('command')
+        
+        #nimmt den Nachrichteninhalt als Liste der Wörter
+        content = message.content
+        wordList = re.sub("[^\w]", " ",  content).split()
+
+        #prüft Nachricht
+        if testmessage(content) == 'good':
+            return
+        elif testmessage(content) == 'bad':
+            text = str(message.author.name + badmessage())
+            await message.channel.send(text)
+            text = str(message.author.name + bad_message_detected() + \
+                       message.channel.name)
+            await give_text_in_bot_channel(client, text)
+        elif testmessage(content) == 'error':
+            text = error1(7)
+            give_text_in_bot_channel(client, text)
+        else:
+            text = error1(8)
+            give_text_in_bot_channel(client, text)
+            
+    on_reaction_add_role(client)
+    #------------------------------------------------------------------
+
+       
+    #lässt den Client mit dem TOKEN laufen
+    client.run(TOKEN)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Friedrich Engels/role_management/on_reaction_add_role.py
@@ -0,0 +1,75 @@
+'''-----------------------------------------------------------------
+Python Bot "Friedrich Engels" Version 01.02.01
+von Mark Rygielski
+
+Aufgabe:
+Wenn im entsprechendem Channel eine Reation hinzugefügt wird, soll der
+Bot dem User eine zur Reaction passende rolle geben
+-----------------------------------------------------------------'''
+
+import discord, os, re
+from discord.utils import get
+
+from serverconfic.ids import Channel_ID
+from textoutput.user_messages import vote_message
+from textoutput.user_messages import added_role
+from textoutput.user_messages import already_has_role_text
+from textoutput.error_messages import error1
+from serverconfic.server import ROLES
+from serverconfic.server import EMOJI_LIST
+from serverconfic.server import give_text_in_bot_channel
+from serverconfic.ids import Server_ID
+from role_management.untis import from_raction_get_role
+from role_management.untis import user_has_role
+from role_management.untis import should_ignore_reaction
+from role_management.untis import give_general_role
+from serverconfic.server import give_text_in_bot_channel
+
+   
+def on_reaction_add_role(client):
+
+    #Die Nachricht, auf die Reagiert werden soll, muss im Cash sein.
+    #Daher muss diese nach dem Neustart erneut gesendet werden:
+    @client.event
+    async def on_ready():
+        channel = client.get_channel(int(Channel_ID('vote')))
+        
+        try:
+            await channel.send(vote_message(1))
+        except PermissionError:
+            text = str(error1(2) + channel.name)
+            give_text_in_bot_channel (client, text)
+            message = await channel.send(vote_message(2))
+        except:
+            text = str(error1(0) + 'on reaction_add_role.on_reaction_add_role')
+            give_text_in_bot_channel (client, text)         
+        
+        message = await channel.send(vote_message(2))
+        guild = client.get_guild(id = int(Server_ID()))
+        number_emoji = len(EMOJI_LIST)
+        
+        
+        for i in range(0, number_emoji):
+            emoji_name = EMOJI_LIST[i]
+            await message.add_reaction(emoji_name)
+            #fügt de Nachricht alle emojis hinzu, die benötigt werden
+
+
+      
+    @client.event
+    async def on_reaction_add(reaction, user):
+        #wird ausgelößt, wenn jemand irgenwo reagiert
+        if not should_ignore_reaction(client, user, reaction):
+            #Wird nur ausgeführt, wenn es erwünscht ist
+            role_name = from_raction_get_role(client, reaction)
+            role = discord.utils.get(user.guild.roles, name=role_name)
+            text = str(added_role()+user.name)
+            channel = client.get_channel(int(Channel_ID('vote')))
+            
+            await give_role(client, user, role, channel, text)
+            give_general_role(client, user, channel)
+            
+        else:
+            return
+
+
